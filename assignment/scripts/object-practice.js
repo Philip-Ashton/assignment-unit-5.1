4803419b4d2318410646bd0e0b37d12b47b2897d
Philip Ashton <philligan1@gmail.com>
2024-10-13 16:38:32 -0500

    #2

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -25,6 +25,9 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+let fullName = [me.firstName, me.lastName];
+console.log (fullName);
+
 
 
 
